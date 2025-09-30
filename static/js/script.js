5e43fcc5e5f75f9166cb604bc229125211a80419
Cesar-Espinoza-Olortegui <espinozaolortegui14@gmail.com>
2025-09-29 22:55:47 -0500

    Se culmina la interface de Login

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
-document.querySelector(".select-trigger").addEventListener("click", function() {
-  document.querySelector(".custom-select").classList.toggle("open");
-});
+  document.querySelector(".select-trigger").addEventListener("click", function() {
+    document.querySelector(".custom-select").classList.toggle("open");
+  });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.getElementById("password");
+    const eyeIcon = document.getElementById("eyeIcon");
+
+    eyeIcon.addEventListener("click", function() {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcon.src = "/static/conten/eyeopen.png"; // ícono ojo abierto
+      } else {
+        passwordInput.type = "password";
+        eyeIcon.src = "/static/conten/eyeclose.svg"; // ícono ojo cerrado
+      }
+    });
+  });
